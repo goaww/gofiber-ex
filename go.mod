3d1c07503e27dc5ef77a3dece1e67eb351b46dc1
Hai Nguyen <nduyhai@users.noreply.github.com>
2020-02-09 12:49:49 +0700

    add example gofiber

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module gofiber-ex
+module github.com/goaww/gofiber-ex
 
 go 1.13
+
+require (
+	github.com/gofiber/fiber v1.4.0
+	github.com/klauspost/compress v1.9.8 // indirect
+	github.com/klauspost/cpuid v1.2.3 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-package gofiber_ex
+package main
+
+import "github.com/gofiber/fiber"
+
+func main() {
+	app := fiber.New()
+
+	app.Static("./public")
+
+	app.Use(func(c *fiber.Ctx) {
+		c.Write("Match anything!\n")
+		c.Next()
+	})
+
+	app.Use("/api", func(c *fiber.Ctx) {
+		c.Write("Match starting with /api\n")
+		c.Next()
+	})
+
+	app.Get("/api/user", func(c *fiber.Ctx) {
+		c.Write("Match exact path /api/user\n")
+	})
+
+	app.Listen(7000)
+}
